e7d9066b6d392b5cd336a5ce134235caa67e1675
jinunzzan <lovebox6607@gmail.com>
2023-05-24 14:23:26 +0900

    Initial Commit

---
+++ MVVMPatternEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPatternEX
+//
+//  Created by Eunchan Kim on 2023/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
